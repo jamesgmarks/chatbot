f9d10fbed6c0f3fe731e186ed9852c8683dfd2ae
James Marks <jmarks@niagaracollege.ca>
2017-02-15 14:22:36 -0500

    Began work on login

---
+++ public/script.js
@@ -0,0 +1,3 @@
+
+
+console.log("Loaded script.js.");
---
+++ server.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// About express-validator: 
+//     https://booker.codes/input-validation-in-express-with-express-validator/
+var validator = require('express-validator');
+
+var guid = function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+app.use(bodyParser.json());
+app.use(validator());
+app.use(express.static('public'));
+
+var liveUsers = [];
+var addLiveUser = function(username, email) {
+    if(liveUsers.findIndex(u => u.username === username) !== -1) {
+        return false;
+    }
+    var newUser = {
+        username, email, token: guid()
+    };
+    console.log(liveUsers.push(newUser) + " users.");
+    console.log(newUser);
+    return newUser;
+}
+
+var loginRouter = express.Router();
+loginRouter.route('')
+    .get(function(req, res) {
+        res.send("get request");
+    })
+    .post(function(req, res) {
+        if(!req.body.username || !req.body.email) {
+            res.statusCode = 401;
+            res.statusMessage = "Not Authorized";
+            res.json({ error: "error", message: "Not Authorized" });
+        }
+        req.checkBody("email", "Enter a valid email address.").isEmail()
+        var errors = req.validationErrors();
+        if(errors) {
+            res.statusCode = 403;
+            res.statusMessage = "Invalid Email Address";
+            res.json({ error: "error", message: "Invalid email address.", errors });
+            return;
+        }
+        res.status(200);
+        var responseJSON = addLiveUser(req.body.username, req.body.email);
+        if(!responseJSON) {
+            res.status(500);
+            res.statusMessage = "Error adding user. Username may already be in use.";
+            res.json({error:"error", message: "Error adding user. Username may already be in use." });
+        } else {
+            res.json(responseJSON);
+        }
+    });
+
+// This is KEY to making a router work. Must "use" by express app.
+app.use('/login', loginRouter);
+
+// TODO: configurable port #
+app.listen(99);
+console.log("App listening at 99.");
